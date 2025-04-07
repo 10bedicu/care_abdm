b19dab2133b4c6853da6e68a52b10e7c0fe9e69a
Khavin Shankar <khavinshankar@gmail.com>
2025-04-07 16:46:30 +0530

    Update periodic task schedule for retry_failed_care_contexts to run at 2:22 AM

--- abdm/tasks/__init__.py
+++ abdm/tasks/__init__.py
@@ -1,3 +1,5 @@
+import random
+
 from celery import current_app
 from celery.schedules import crontab
 
@@ -7,7 +9,7 @@ from abdm.tasks.retry_failed_care_contexts import retry_failed_care_contexts
 @current_app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
-        crontab(hour="0", minute="0"),
+        crontab(hour="2", minute="22"),
         retry_failed_care_contexts.s(),
         name="retry_failed_care_contexts",
     )
