8bb95a3e15a588d709ec6bd90e4fc3a0e1f2db39
siva222003 <sivapidugu02@gmail.com>
2025-04-10 03:40:46 +0530

    add patient notify fn & map responses to wellness records

--- abdm/signals/register_care_contexts.py
+++ abdm/signals/register_care_contexts.py
@@ -8,6 +8,7 @@ from abdm.models import HealthInformationType
 from abdm.service.helper import ABDMAPIException
 from abdm.service.v3.gateway import GatewayService
 from care.emr.models.medication_request import MedicationRequest
+from care.emr.models.questionnaire import QuestionnaireResponse
 
 logger = logging.getLogger(__name__)
 
@@ -52,4 +53,45 @@ def create_care_context_on_medication_request_creation(
 
     except Exception as e:
         warning = f"Failed to link care context for medication request {instance.external_id} with patient {patient.external_id}, {e!s}"
+        logger.exception(warning)
+
+
+@receiver(post_save, sender=QuestionnaireResponse)
+def create_care_context_on_questionnaire_response_creation(
+    sender, instance: QuestionnaireResponse, created: bool, **kwargs
+):
+    patient = instance.patient
+    encounter = instance.encounter
+
+    if (
+        not created
+        or not patient
+        or not encounter
+        or instance.structured_response_type is not None
+    ):
+        return
+
+    try:
+        transaction.on_commit(
+            lambda: GatewayService.link__carecontext(
+                {
+                    "patient": patient,
+                    "encounter": encounter,
+                    "care_contexts": [
+                        {
+                            "hi_type": HealthInformationType.WELLNESS_RECORD,
+                            "reference": f"v2::observation::{instance.external_id}",
+                            "display": f"Observation Recorded on {instance.created_date.date()}",
+                        }
+                    ],
+                    "user": instance.created_by,
+                }
+            )
+        )
+    except ABDMAPIException as e:
+        warning = f"Failed to link care context for questionnaire response {instance.external_id} with patient {patient.external_id}, {e.detail!s}"
+        logger.warning(warning)
+
+    except Exception as e:
+        warning = f"Failed to link care context for questionnaire response {instance.external_id} with patient {patient.external_id}, {e!s}"
         logger.exception(warning)
