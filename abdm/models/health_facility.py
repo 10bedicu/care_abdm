6f1b8859e99c2aaa57f73a69832d73f3fe652380
Khavin Shankar <khavinshankar@gmail.com>
2025-05-30 12:54:53 +0530

    update hf_id in transactions when hf_id is updated

--- abdm/models/health_facility.py
+++ abdm/models/health_facility.py
@@ -1,7 +1,8 @@
-from care.utils.models.base import BaseModel
-from django.db import models
+from django.db import models, transaction
 
 from abdm.models.permissions.health_facility import HealthFacilityPermissions
+from abdm.service.helper import ABDMAPIException, update_hf_id_in_transactions
+from care.utils.models.base import BaseModel
 
 
 class HealthFacility(BaseModel, HealthFacilityPermissions):
@@ -10,6 +11,21 @@ class HealthFacility(BaseModel, HealthFacilityPermissions):
     facility = models.OneToOneField(
         "facility.Facility", on_delete=models.PROTECT, to_field="external_id"
     )
+
+    def save(self, *args, **kwargs):
+        with transaction.atomic():
+            if self.pk:
+                old_instance = HealthFacility.objects.get(pk=self.pk)
+                if old_instance.hf_id != self.hf_id:
+                    try:
+                        update_hf_id_in_transactions(
+                            old_instance.hf_id, self.hf_id, batch_size=1000
+                        )
+                    except Exception as e:
+                        raise ABDMAPIException(
+                            detail=f"Failed to update transactions for hf_id change: {e!s}"
+                        ) from e
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.hf_id} {self.facility}"
