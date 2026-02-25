8dc724d3360a98cb0d28e207e71f8565b49c8ac1
Aakash Singh <mail@singhaakash.dev>
2026-02-25 15:11:48 +0530

    refactor: update patient identifier config retrieval to use filter and create if not exists

--- abdm/api/v3/viewsets/health_id.py
+++ abdm/api/v3/viewsets/health_id.py
@@ -123,13 +123,15 @@ class HealthIdViewSet(GenericViewSet):
 
         abdm_user = get_or_create_abdm_user()
 
-        patient_identifier_config, _ = PatientIdentifierConfig.objects.get_or_create(
+        patient_identifier_config = PatientIdentifierConfig.objects.filter(
             config__system=settings.ABHA_NUMBER_IDENTIFIER_SYSTEM_SYSTEM,
-            facility=None,
-            created_by=abdm_user,
-            defaults={
-                "status": "active",
-                "config": {
+        ).first()
+        if not patient_identifier_config:
+            patient_identifier_config = PatientIdentifierConfig.objects.create(
+                status="active",
+                facility=None,
+                created_by=abdm_user,
+                config={
                     "use": "official",
                     "description": settings.ABHA_NUMBER_IDENTIFIER_SYSTEM_DISPLAY,
                     "required": False,
@@ -143,10 +145,7 @@ class HealthIdViewSet(GenericViewSet):
                         "retrieve_with_otp": False,
                     },
                 },
-                "facility": None,
-                "created_by": abdm_user,
-            },
-        )
+            )
 
         PatientIdentifier.objects.create(
             patient=patient,
